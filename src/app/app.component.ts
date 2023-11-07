6c7b2bfc8fadee2048fec2a5fc7cde9f0049744d
dongsulee <dongsulee@nextedition.co.kr>
2023-11-07 14:11:44 +0900

    feat : tutorial1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
+
+// selector: Angular가 컴포넌트를 어떻게 참조하지 방법을 설명합니다.
+// standalone: 컴포넌트에 ngModule이 필요한지 여부를 설명합니다.
+// imports: 컴포넌트의 종속성을 설명합니다.
+// template: 컴포넌트의 HTML 마크업 및 레이아웃을 설명합니다.
+// styleUrls: 컴포넌트가 배열에서 사용하는 CSS 파일의 URL을 나열합니다.
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "default";
+  title = "집";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { HousingLocation } from "../housinglocation";
+import { HousingService } from "../housing.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-home",
@@ -10,14 +12,53 @@ import { HousingLocation } from "../housinglocation";
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <!-- <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button> -->
+
+        <input type="text" placeholder="도시 필터링하기" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          검색
+        </button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <!-- <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      >
+      </app-housing-location> -->
+
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
