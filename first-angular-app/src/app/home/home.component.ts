612f5f98a85850f009b93a0369c9a08e870f9471
dongsu <sonicce99@naver.com>
2023-12-03 11:28:15 +0900

    chore : first-angular-app 폴더로 이동

---
+++ first-angular-app/src/app/home/home.component.ts
@@ -0,0 +1,66 @@
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housinglocation";
+import { HousingService } from "../housing.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+
+@Component({
+  selector: "app-home",
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <!-- <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button> -->
+
+        <!-- <input type="text" placeholder="도시 필터링하기" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          검색
+        </button> -->
+
+        <input type="date" min="2023-12-05" ng-model="2023-12-08" />
+      </form>
+    </section>
+    <section class="results">
+      <!-- <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      >
+      </app-housing-location> -->
+
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
